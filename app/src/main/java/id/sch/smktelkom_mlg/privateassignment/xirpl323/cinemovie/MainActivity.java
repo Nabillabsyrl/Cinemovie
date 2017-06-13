95769a451bf68af05e897b4bf583ea07684e3829
Nabilla Basharahil <nabillabasharahil@gmail>
2017-06-13 10:51:38 +0700

    done db

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl323/cinemovie/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl323/cinemovie/MainActivity.java
@@ -33,6 +33,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        changePage(R.id.nav_slideshow);
+        navigationView.setCheckedItem(R.id.nav_slideshow);
     }
 
     @Override
